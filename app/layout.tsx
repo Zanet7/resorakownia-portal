f124ae8a033431928d8d8ec2a6669448f89d313c
zaneta <zanetagrzes7@gmail.com>
2026-04-11 13:03:13 +0200

    edit 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Resorakownia",
@@ -9,6 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pl">
       <body>
+        <Navbar />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   );
