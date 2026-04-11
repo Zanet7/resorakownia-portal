67e2d4b98813ddefe8889b2433dcad200a81f428
zaneta <zanetagrzes7@gmail.com>
2026-04-11 22:49:58 +0200

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createBrowserClient } from "@supabase/ssr";
 
 export default function LoginPage() {
-  const supabase = createClientComponentClient();
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
