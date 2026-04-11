eb3706e71e7a474eab9db3896de55076d1ba0290
zaneta <zanetagrzes7@gmail.com>
2026-04-11 12:01:16 +0200

    Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-4 bg-gray-900 text-white text-center">
+      <p>© 2026 Resorakownia. All rights reserved.</p>
+    </footer>
+  );
+}
