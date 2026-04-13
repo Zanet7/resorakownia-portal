da8a94264d1a7d1c888b7bc70b64c78259e51199
zaneta <zanetagrzes7@gmail.com>
2026-04-13 11:54:45 +0200

    changes

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -7,14 +7,17 @@ export function supabaseServer() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookies().get(name)?.value;
+        async get(name: string) {
+          const store = await cookies();
+          return store.get(name)?.value;
         },
-        set(name: string, value: string, options: any) {
-          cookies().set(name, value, options);
+        async set(name: string, value: string, options: any) {
+          const store = await cookies();
+          store.set(name, value, options);
         },
-        remove(name: string, options: any) {
-          cookies().set(name, "", options);
+        async remove(name: string, options: any) {
+          const store = await cookies();
+          store.set(name, "", options);
         },
       },
     }
